3fd9e12b8b6911c5f053da8df00b9714b0fc42df
Daniel Irvine <daniel@danielirvine.com>
2020-01-10 23:11:42 +0000

    Replace single quotes with double quotes

--- rollup.test.config.js
+++ rollup.test.config.js
@@ -1,22 +1,22 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import multi from '@rollup/plugin-multi-entry';
-import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import multi from "@rollup/plugin-multi-entry";
+import svelte from "rollup-plugin-svelte";
+import babel from "rollup-plugin-babel";
 
 export default {
-  input: 'spec/**/*.spec.js',
+  input: "spec/**/*.spec.js",
   output: {
     sourcemap: true,
-    format: 'cjs',
-    name: 'tests',
-    file: 'build/bundle-tests.js'
+    format: "cjs",
+    name: "tests",
+    file: "build/bundle-tests.js"
   },
   plugins: [
     multi(),
     svelte({ css: false, dev: true }),
     resolve({
-      dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/'),
+      dedupe: importee => importee === "svelte" || importee.startsWith("svelte/"),
       only: [/^svelte/]
     }),
     commonjs(),
@@ -26,7 +26,7 @@ export default {
     })
   ],
   onwarn (warning, warn) {
-    if (warning.code === 'UNRESOLVED_IMPORT') return;
+    if (warning.code === "UNRESOLVED_IMPORT") return;
     warn(warning);
   }
 };
