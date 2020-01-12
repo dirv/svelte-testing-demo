2d0ae9612198ae33b9e9fc68046fe5123304248b
Daniel Irvine <daniel@danielirvine.com>
2020-01-12 02:11:53 +0000

    Add a test for onMount

--- spec/CallbackComponent.spec.js
+++ spec/CallbackComponent.spec.js
@@ -1,19 +1,22 @@
 import { mount, asSvelteComponent } from "./support/svelte.js";
 import { tick } from "svelte";
-import { price } from "../src/stores/price.js";
+import {
+  price,
+  fetch as fetchPrice,
+  rewire$fetch,
+  restore
+} from "../src/stores/price.js";
 import CallbackComponent from "../src/CallbackComponent.svelte";
-
-const fetchOkResponse = data =>
-  Promise.resolve({ ok: true, json: () => Promise.resolve(data) });
 
 describe(CallbackComponent.name, () => {
   asSvelteComponent();
 
   beforeEach(() => {
-    global.window = {};
-    global.window.fetch = () => ({});
-    spyOn(window, "fetch")
-      .and.returnValue(fetchOkResponse({ price: 99.99 }));
+    rewire$fetch(jasmine.createSpy());
+  });
+
+  afterEach(() => {
+    restore();
   });
 
   it("displays the initial price", () => {
@@ -27,5 +30,10 @@ describe(CallbackComponent.name, () => {
     price.set(123.45);
     await tick();
     expect(container.textContent).toContain("The price is: $123.45");
+  });
+
+  it("fetches prices on mount", () => {
+    mount(CallbackComponent);
+    expect(fetchPrice).toHaveBeenCalled();
   });
 });
