f286b0827fabd6e5c6ef3257ef34d306124cb67e
Daniel Irvine <daniel@danielirvine.com>
2020-10-06 09:35:11 +0100

    Upgrade dependencies

--- rollup.test.config.js
+++ rollup.test.config.js
@@ -1,11 +1,11 @@
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import multi from "@rollup/plugin-multi-entry";
 import svelte from "rollup-plugin-svelte";
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 
 export default {
-  input: "spec/**/*.spec.js",
+  //input: "spec/**/*.spec.js",
+  input: "spec/CallbackComponent.spec.js",
   output: {
     sourcemap: true,
     format: "cjs",
@@ -15,11 +15,9 @@ export default {
   plugins: [
     multi(),
     svelte({ css: false, dev: true }),
-    resolve({
-      only: [/^svelte-/]
-    }),
     commonjs(),
     babel({
+      babelHelpers: "bundled",
       extensions: [".js", ".svelte"],
       plugins: ["rewire-exports"]
     })
